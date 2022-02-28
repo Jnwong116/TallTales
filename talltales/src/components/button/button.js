541f0d336bf16ce0db85c1edc2fc3571fdeb87b4
jaspermai <jaspermai25@gmail.com>
2022-02-27 22:59:28 -0500

    [Backstory] Button component create

---
+++ talltales/src/components/button/button.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./button.css";
+
+class Button extends React.Component {
+    
+    
+    render() {
+        const { text, handleClick } = this.props;
+
+        return (
+            <button onClick={handleClick} className='buttonStyle'>{text}</button>
+        )
+    }
+}
+
+export default Button;
--- talltales/src/pages/inputStage/inputStage.js
+++ talltales/src/pages/inputStage/inputStage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AppName from '../../components/appName/appName.js';
+import Button from '../../components/button/button.js';
 
 class InputStage extends React.Component {
   constructor() {
@@ -15,10 +16,17 @@ class InputStage extends React.Component {
       activeKey: eventKey
     });
   }
+  handleClick() {
+    console.log("send");
+    
+    }
+
   render() {
+
     return (
         <div>
             <AppName></AppName>
+            <Button text="SEND" handleClick={this.handleClick}></Button>
         </div>
     );
   }
