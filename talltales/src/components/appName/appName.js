c089fcbcd1709e02da9d0861a0684794b68ea029
jaspermai <jaspermai25@gmail.com>
2022-02-27 21:56:11 -0500

    [backstory] appName components create

---
+++ talltales/src/components/appName/appName.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./appName_style.css";
+
+class AppName extends React.Component {
+    
+    render() {
+        return (
+            <p className='name'>Tall Tales</p>
+        )
+    }
+}
+
+export default AppName;
