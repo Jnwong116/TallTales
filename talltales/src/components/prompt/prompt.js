1787ca8dc0a0f2062d5ef15c21a90b868c831666
Jordan <jnwong116@gmail.com>
2022-02-27 20:35:10 -0500

    Set up components for inputStage

---
+++ talltales/src/components/prompt/prompt.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./prompt_style.css";
+
+class Prompt extends React.Component {
+    
+    
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Prompt;
