c50e42cc1ffb32acf15a86cb56c2657a97966997
Chris <chriskorosu@gmail.com>
2022-03-01 12:02:44 -0500

    Minor labelling changes (code smells)

--- talltales/src/components/userInput/userInput.js
+++ talltales/src/components/userInput/userInput.js
@@ -8,16 +8,16 @@ class UserInput extends React.Component {
   render() {
     return (
       <div className="user-input">
-        <div className="user-input-title">TYPE YOUR SENTENCE</div>
-        <div className="flex-container">
-            <img src={rightArrow} className="right-arrow" alt="Right Arrow"/>
+        <div className="user-input-header">TYPE YOUR SENTENCE</div>
+        <div className="stage-indicator">
+            <img src={rightArrow} className="stage-arrow" alt="Right Arrow"/>
             <div className="user-input-tag">{this.props.tag}</div>
         </div>
-        <div className="flex-container user-input-field">
+        <div className="stage-indicator user-input-field">
             <UserIcon user={this.props.user}></UserIcon>
-            <TextField 
-                id="outlined-basic" 
-                label="OKAY AND THEN..." 
+            <TextField
+                id="outlined-basic"
+                label="OKAY AND THEN..."
                 variant="filled"
                 margin="normal"
                 multiline="true"
