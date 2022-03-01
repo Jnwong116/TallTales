d5e519eb73ad7ba93274a0181ebe0409ad6acf69
Chris <chriskorosu@gmail.com>
2022-02-28 19:15:11 -0500

    set up userIcon component

---
+++ talltales/src/components/userIcon/userIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+import avatar from '../../data/images/avatars/avatar01.png';
+import "./userIcon_style.css";
+
+class UserIcon extends React.Component {
+  render() {
+    return (
+      <div className="UserIcon">
+          <img src={avatar} className="avatar" alt="Avatar" />
+          
+      </div>
+    );
+  }
+}
+
+export default UserIcon;
