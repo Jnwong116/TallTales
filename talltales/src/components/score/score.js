e0e4ee8bec2a226ec5c1cf58ddfaf71eb7f97704
Jordan <jnwong116@gmail.com>
2022-02-28 20:37:57 -0500

    Score changes with mock data

--- talltales/src/components/score/score.js
+++ talltales/src/components/score/score.js
@@ -6,7 +6,7 @@ class Score extends React.Component {
   render() {
     return (
       <div className="score">
-        <span>score: {this.props.score}</span>
+        <span>score: {this.props.user.score}</span>
       </div>
     );
   }
--- talltales/src/pages/inputStage/inputStage.js
+++ talltales/src/pages/inputStage/inputStage.js
@@ -7,31 +7,35 @@ import UserInput from "../../components/userInput/userInput.js";
 import UserIcon from "../../components/userIcon/userIcon.js";
 
 class InputStage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      expanded: true,
-      activeKey: "1"
-    };
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-  handleSelect(eventKey) {
-    this.setState({
-      activeKey: eventKey
-    });
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     expanded: true,
+  //     activeKey: "1"
+  //   };
+  //   this.handleSelect = this.handleSelect.bind(this);
+  // }
+  // handleSelect(eventKey) {
+  //   this.setState({
+  //     activeKey: eventKey
+  //   });
+  // }
+
   handleClick() {
     console.log("send");
     
     }
 
   render() {
+    // Import mock data
+    this.stories = require('./../../data/stories.json');
+    this.users = require('./../../data/users.json');
 
     return (
       <div className="input-stage">
         <div className="input-stage-header">
           <AppName></AppName>
-          <Score score={0}></Score>
+          <Score user={this.users.users[0]}></Score>
         </div>
         <UserInput tag="Set up the backstory!"></UserInput>
         <UserIcon />
