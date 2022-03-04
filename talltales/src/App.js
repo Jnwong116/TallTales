b45867bc3877ffd4a3e005725172a84567b915ec
jwong8448 <jnwong116@gmail.com>
2022-03-03 21:35:24 -0500

    Added to router

--- talltales/src/App.js
+++ talltales/src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/gameStage" element={<InputStage />} />
+          <Route path="/voteStage" element={<VoteStage />} />
         </Switch>
       </Router>
     </div>
