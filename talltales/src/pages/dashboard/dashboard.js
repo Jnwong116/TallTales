5a67b94885cbb5d7dc6012b1e9d475fc25f3022b
Chris <chriskorosu@gmail.com>
2022-03-04 05:01:23 -0500

    Added user icon, modified layout

--- talltales/src/pages/dashboard/dashboard.js
+++ talltales/src/pages/dashboard/dashboard.js
@@ -15,38 +15,40 @@ class Dashboard extends React.Component {
         <div className="header">
           <AppName></AppName>
         </div>
-        <div className="dashboardMenuContainer">
-          <div>
-            
+        <div className="dashboardMenuOuterContainer">
+          <div className="userIconContainer">
+            <UserIcon icon="avatar01.png" username="Welcome back, gazi!" />
           </div>
-          <span className="dashboardMenu">
-            <Button text="HOST NEW GAME"
-                    handleClick={() => {
-                    this.handleClick(this.state);}} />
-            <Button text="CHANGE USERNAME"
-                    handleClick={() => {
+          <div className="dashboardMenuInnerContainer">
+            <span className="dashboardMenu">
+              <Button text="HOST NEW GAME"
+                      handleClick={() => {
                       this.handleClick(this.state);}} />
-            <Button text="LEADERBOARD"
+              <Button text="CHANGE USERNAME"
+                      handleClick={() => {
+                        this.handleClick(this.state);}} />
+              <Button text="LEADERBOARD"
+                      handleClick={() => {
+                      this.handleClick(this.state);}} />
+              <Button text="HOW TO PLAY"
+                      handleClick={() => {
+                      this.handleClick(this.state);}} />
+            </span>
+            <span className="dashboardMenu">
+              <Button text="JOIN GAME"
+                      handleClick={() => {
+                      this.handleClick(this.state);}} />
+              <Button text="CHANGE PASSWORD"
+                      handleClick={() => {
+                      this.handleClick(this.state);}} />
+              <Button text="BROWSE PROFILES"
                     handleClick={() => {
                     this.handleClick(this.state);}} />
-            <Button text="HOW TO PLAY"
-                    handleClick={() => {
-                    this.handleClick(this.state);}} />
-          </span>
-          <span className="dashboardMenu">
-            <Button text="JOIN GAME"
-                    handleClick={() => {
-                    this.handleClick(this.state);}} />
-            <Button text="CHANGE PASSWORD"
-                    handleClick={() => {
-                    this.handleClick(this.state);}} />
-            <Button text="BROWSE PROFILES"
-                  handleClick={() => {
-                  this.handleClick(this.state);}} />
-            <Button text="LOG OUT"
-                    handleClick={() => {
-                    this.handleClick(this.state);}} />
-          </span>
+              <Button text="LOG OUT"
+                      handleClick={() => {
+                      this.handleClick(this.state);}} />
+            </span>
+          </div>
         </div>
       </div>
     );
