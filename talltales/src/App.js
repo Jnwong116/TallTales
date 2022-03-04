a0bf9fdd4166eeb66c2e3e0b21508ae5c2e4fa4c
jwong8448 <jnwong116@gmail.com>
2022-03-03 20:43:38 -0500

    Set up react router

--- talltales/src/App.js
+++ talltales/src/App.js
@@ -5,13 +5,19 @@ import Register from "./pages/register/register"
 import "./App.css";
 import Lobby from "./pages/lobby/lobby";
 
+import { BrowserRouter as Router, Routes as Switch, Route, Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      {/* <InputStage></InputStage> */}
-      <Lobby></Lobby>
-      {/* <Login></Login> */}
-      {/* <Register></Register> */}
+      <Router>
+        <Switch>
+          <Route path="/lobby" element={<Lobby />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/gameStage" element={<InputStage />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
