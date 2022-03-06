06af94bac1746f7e305ace717d57ad5014a8aec4
Chris <chriskorosu@gmail.com>
2022-03-06 13:01:49 -0500

    Dashboard: Removed obsolete commented out code

--- talltales/src/pages/dashboard/dashboard.js
+++ talltales/src/pages/dashboard/dashboard.js
@@ -61,47 +61,6 @@ class Dashboard extends React.Component {
             Chris and Jordan are trying to turn random cans of food into something remotely tasty. When most canned “food” is either pet food or well past its expiration date (or both), they’ve got to turn to other means. This is all happening on a fishing boat in northern Canada. Gazi and Jasper watch on, bemused. No one thought to bring a can opener with them.  Chris reminds everyone he's on a plant-based diet. A hostile, carnivorous monkey attacks Chris shortly after this proclamation. A swarm of rats emerges from the lower decks and start chipping the cans open. Jordan throws a can at the monkey, scoring a direct hit. The monkey faints and falls off a slightly chewed-on Chris.  While this was happening, the rats opened all the cans and carried away the pet food. Luckily, since northern Canada is basically a freezer, the expiration date had no effect, so the remaining food is edible, with plenty of plant-based options. Chris and Jasper eat canned sauerkraut, while Jordan and Gazi cook up some monkey stew.
           </div>
         </span>
-
-
-
-
-        {/*
-        <div className="dashboardMenuOuterContainer">
-          <div className="userIconContainer">
-            <UserIcon icon="avatar01.png" username="Welcome back, gazi!" />
-          </div>
-          <div className="dashboardMenuInnerContainer">
-            <span className="dashboardMenu">
-              <Button text="HOST NEW GAME"
-                      handleClick={() => {
-                      this.handleClick(this.state);}} />
-              <Button text="CHANGE USERNAME"
-                      handleClick={() => {
-                        this.handleClick(this.state);}} />
-              <Button text="LEADERBOARD"
-                      handleClick={() => {
-                      this.handleClick(this.state);}} />
-              <Button text="HOW TO PLAY"
-                      handleClick={() => {
-                      this.handleClick(this.state);}} />
-            </span>
-            <span className="dashboardMenu">
-              <Button text="JOIN GAME"
-                      handleClick={() => {
-                      this.handleClick(this.state);}} />
-              <Button text="CHANGE PASSWORD"
-                      handleClick={() => {
-                      this.handleClick(this.state);}} />
-              <Button text="BROWSE PROFILES"
-                    handleClick={() => {
-                    this.handleClick(this.state);}} />
-              <Button text="LOG OUT"
-                      handleClick={() => {
-                      this.handleClick(this.state);}} />
-            </span>
-          </div>
-        </div>
-      */}
       </div>
     );
   }
