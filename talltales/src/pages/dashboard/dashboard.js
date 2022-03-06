b71d3a850e43a382a59d626f73e1b7f24e900b68
Chris <chriskorosu@gmail.com>
2022-03-05 23:33:20 -0500

    Re-designed basic dashboard layout

--- talltales/src/pages/dashboard/dashboard.js
+++ talltales/src/pages/dashboard/dashboard.js
@@ -12,9 +12,55 @@ class Dashboard extends React.Component {
 
     return (
       <div className="dashboard">
-        <div className="header">
-          <AppName></AppName>
-        </div>
+        <span className="dashboardLeft">
+          <div className="header">
+            <AppName></AppName>
+          </div>
+          <span className="dashboardMenu">
+            <Button text="HOST NEW GAME"
+                    handleClick={() => {
+                    this.handleClick(this.state);}} />
+            <Button text="CHANGE USERNAME"
+                    handleClick={() => {
+                      this.handleClick(this.state);}} />
+            <Button text="LEADERBOARD"
+                    handleClick={() => {
+                    this.handleClick(this.state);}} />
+            <Button text="HOW TO PLAY"
+                    handleClick={() => {
+                    this.handleClick(this.state);}} />
+          </span>
+        </span>
+
+        <span className="dashboardDivider">
+        </span>
+
+        <span className="dashboardRight">
+          <div className="userIconContainer">
+            <div className="userIconContainer">
+              <UserIcon icon="avatar01.png" username="Welcome back, gazi!" />
+            </div>
+          </div>
+          <div className="storiesNavigation">
+            <span className="browseStoriesLeftArrow">
+              &lt;
+            </span>
+            <span className="browseStoriesHeader">
+              Latest Stories
+            </span>
+            <span className="browseStoriesLeftArrow">
+              &gt;
+            </span>
+          </div>
+          <div className="storiesDisplay">
+            Chris and Jordan are trying to turn random cans of food into something remotely tasty. When most canned “food” is either pet food or well past its expiration date (or both), they’ve got to turn to other means. This is all happening on a fishing boat in northern Canada. Gazi and Jasper watch on, bemused. No one thought to bring a can opener with them.  Chris reminds everyone he's on a plant-based diet. A hostile, carnivorous monkey attacks Chris shortly after this proclamation. A swarm of rats emerges from the lower decks and start chipping the cans open. Jordan throws a can at the monkey, scoring a direct hit. The monkey faints and falls off a slightly chewed-on Chris.  While this was happening, the rats opened all the cans and carried away the pet food. Luckily, since northern Canada is basically a freezer, the expiration date had no effect, so the remaining food is edible, with plenty of plant-based options. Chris and Jasper eat canned sauerkraut, while Jordan and Gazi cook up some monkey stew.
+          </div>
+        </span>
+
+
+
+
+        {/*
         <div className="dashboardMenuOuterContainer">
           <div className="userIconContainer">
             <UserIcon icon="avatar01.png" username="Welcome back, gazi!" />
@@ -50,6 +96,7 @@ class Dashboard extends React.Component {
             </span>
           </div>
         </div>
+      */}
       </div>
     );
   }
