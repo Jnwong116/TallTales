d83ed3325a532171033cdc0d781f6b631f516008
Jordan <jnwong116@gmail.com>
2022-03-06 21:04:33 -0500

    Host game works

--- talltales/src/actions/dashboard/menu.js
+++ talltales/src/actions/dashboard/menu.js
@@ -10,3 +10,30 @@ export const logout = (app) => {
         page: 0
     })
 }
+
+export const startGame = (host, users, app, page) => {
+    if (host) {
+        for (let i = 0; i < users.users.length; i++) {
+            if (users.users[i].username === app.state.currUser.username) {
+                users.users[i].host = true;
+            }
+        }
+
+        menuRedirect(app, page);
+    }   
+
+    else {
+        let hosted = false;
+        for (let i = 0; i < users.users.length; i++) {
+            if (hosted === false) {
+                if (users.users[i].username !== app.state.currUser.username) {
+                    users.users[i].host = true;
+                    hosted = true;
+                }
+            }
+            
+        }
+
+        menuRedirect(app, page);
+    }
+}
