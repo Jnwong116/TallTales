64f09efa79b41acb3ccead9a84df9fc847971587
jaspermai <jaspermai25@gmail.com>
2022-04-04 12:55:57 -0400

    host crown

--- client/src/components/userIcon/userIcon.js
+++ client/src/components/userIcon/userIcon.js
@@ -5,8 +5,15 @@ class UserIcon extends React.Component {
   render() {
     return (
       <div className="user-icon">
-        <img src={require("../../assets/images/" + this.props.icon)} alt="img"></img>
-        {this.props.username}
+        <div className="user-icon-icon">
+            <img src={require("../../assets/images/" + this.props.icon)} alt="img"></img>
+        </div>
+        <div className="user-icon-username">
+            {this.props.username}
+            {(this.props.host) ? 
+                <img src={require("../../assets/images/crown.png")} alt="crown"></img> : undefined
+            }
+        </div>
       </div>
     );
   }
