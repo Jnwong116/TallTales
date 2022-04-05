b7a561b5d6d4ce4c6dcafe069dc5a1a4dd85a363
Jordan <jnwong116@gmail.com>
2022-04-05 02:46:21 -0400

    Updated sockets to reflect music changes (for real

--- client/src/components/muteButton/muteButton.js
+++ client/src/components/muteButton/muteButton.js
@@ -1,28 +1,15 @@
 import React from "react";
+import { handleMute } from "../../actions/mute/muteGame";
 
 import "./muteButton.css";
 
 class MuteButton extends React.Component {
     render() {
-
-        const audioRef = this.props.audioRef;
-
-        const handleMute = () => {
-            if(this.props.app.state.muted) {
-                this.props.app.setState( { muted: false });
-                audioRef.audioEl.current.muted = false;
-            }
-            else {
-                this.props.app.setState( { muted: true });
-                audioRef.audioEl.current.muted = true;
-            }
-        }
-
         return (
             <div className="mute-button">
                 {(this.props.app.state.muted) ? 
-                    <img src={require("../../assets/images/volume_off.png")} alt="volume" onClick={() => handleMute()}></img> :
-                    <img src={require("../../assets/images/volume_on.png")} alt="volume" onClick={() => handleMute()}></img>
+                    <img src={require("../../assets/images/volume_off.png")} alt="volume" onClick={() => handleMute(this.props.app, this.props.audioRef)}></img> :
+                    <img src={require("../../assets/images/volume_on.png")} alt="volume" onClick={() => handleMute(this.props.app, this.props.audioRef)}></img>
                 } 
             </div>
         )
